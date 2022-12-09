c35f453c9480cf884a61494eddc49ea06a710508
bazilik <bazilik@templatemonster.me>
2022-12-09 13:00:47 +0200

    UPD: add reload handlers

--- assets/config.js
+++ assets/config.js
@@ -1,1 +1,27 @@
-console.log('111');
+( function ( $ ) {
+
+	$(document).ready(function(){
+
+		let reload = $(".reload-on-interval");
+
+		reload.each(function() {
+
+			let classes = $( this ).attr('class').match(/\interval--\S+/g);
+
+			let classesToString = classes.toString(); 
+
+			let interval =  parseInt(classesToString.match(/\d+/));
+
+			setInterval(reloadOnPage, interval);
+
+			function reloadOnPage() {
+				window.JetEngine.ajaxGetListing;
+			}
+
+		});
+
+	});
+
+} ( jQuery ) );
+
+
