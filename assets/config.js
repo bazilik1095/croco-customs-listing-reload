46d6f1fcd71c98ba432a0c37f489b46bc52f00db
bazilik <bazilik@templatemonster.me>
2022-12-12 01:01:28 +0200

    UPD: fix

--- assets/config.js
+++ assets/config.js
@@ -6,15 +6,35 @@
 
 		reload.each(function() {
 
-			let classes = $( this ).attr('class').match(/\interval--\S+/g);
+			let $this = $( this ),
+			
+			classes = $this.attr('class').match(/\interval--\S+/g),
+			
+			classesToString = classes.toString(),
+			
+			interval =  parseInt(classesToString.match(/\d+/));
 
-			let classesToString = classes.toString(); 
+			let args = [
+				handler = 'get_listing',
+				container = $this,
+				masonry = false,
+				slider = false,
+				append = false,
+				items = $this.find( '.jet-listing-grid__items' ),
+				nav   = items.data( 'nav' ),
+				query = nav.query,
+				widget_settings = nav.widget_settings,
+				postID    = window.elementorFrontendConfig.post.id,
+				elementID = $this.data( 'id' )
+			];
 
-			let interval =  parseInt(classesToString.match(/\d+/));
+			console.log(args);
 
 			setInterval(function() {
-			      window.JetEngine.ajaxGetListing();
+			     window.JetEngine.ajaxGetListing(args);
 			}, interval*1000);
+
+			console.log(window.JetEngine.ajaxGetListing(args));
 
 		});
 
